312c3684576cb58f780b91d206b41ebf869c118b
Ryan Lynch <rynplynch@gmail.com>
2024-07-16 11:54:58 -0700

    formating and comments

--- hw-2a/sthread.cpp
+++ hw-2a/sthread.cpp
@@ -1,20 +1,22 @@
+#include <iostream> // cout
+#include <queue>    // queue
 #include <setjmp.h> // setjmp( )
 #include <signal.h> // signal( )
-#include <unistd.h> // sleep( ), alarm( )
 #include <stdio.h>  // perror( )
 #include <stdlib.h> // exit( )
-#include <iostream> // cout
 #include <string.h> // memcpy
-#include <queue>    // queue
+#include <unistd.h> // sleep( ), alarm( )
 
-#define scheduler_init( ) {			\
-    if ( setjmp( main_env ) == 0 )		\
-      scheduler( );				\
+#define scheduler_init()                                                       \
+  {                                                                            \
+    if (setjmp(main_env) == 0)                                                 \
+      scheduler();                                                             \
   }
 
-#define scheduler_start( ) {			\
-    if ( setjmp( main_env ) == 0 )		\
-      longjmp( scheduler_env, 1 );		\
+#define scheduler_start()                                                      \
+  {                                                                            \
+    if (setjmp(main_env) == 0)                                                 \
+      longjmp(scheduler_env, 1);                                               \
   }
 
 // TODO: to be implemented in this assignment
@@ -25,28 +27,31 @@
 #define sthread_yield( ) {						\
   }
 
-#define sthread_init( ) {					\
-    if ( setjmp( cur_tcb->env ) == 0 ) {			\
-      capture( );						\
-      longjmp( main_env, 1 );					\
-    }								\
-    memcpy( cur_tcb->sp, cur_tcb->stack, cur_tcb->size );	\
+#define sthread_init()                                                         \
+  {                                                                            \
+    if (setjmp(cur_tcb->env) == 0) {                                           \
+      capture();                                                               \
+      longjmp(main_env, 1);                                                    \
+    }                                                                          \
+    memcpy(cur_tcb->sp, cur_tcb->stack, cur_tcb->size);                        \
   }
 
-#define sthread_create( function, arguments ) { \
-    if ( setjmp( main_env ) == 0 ) {		\
-      func = &function;				\
-      args = arguments;				\
-      thread_created = true;			\
-      cur_tcb = new TCB( );			\
-      longjmp( scheduler_env, 1 );		\
-    }						\
+#define sthread_create(function, arguments)                                    \
+  {                                                                            \
+    if (setjmp(main_env) == 0) {                                               \
+      func = &function;                                                        \
+      args = arguments;                                                        \
+      thread_created = true;                                                   \
+      cur_tcb = new TCB();                                                     \
+      longjmp(scheduler_env, 1);                                               \
+    }                                                                          \
   }
 
-#define sthread_exit( ) {			\
-    if ( cur_tcb->stack != NULL )		\
-      free( cur_tcb->stack );			\
-    longjmp( scheduler_env, 1 );		\
+#define sthread_exit()                                                         \
+  {                                                                            \
+    if (cur_tcb->stack != NULL)                                                \
+      free(cur_tcb->stack);                                                    \
+    longjmp(scheduler_env, 1);                                                 \
   }
 
 using namespace std;
@@ -57,59 +62,69 @@ static jmp_buf scheduler_env;
 // Thread control block
 class TCB {
 public:
-  TCB( ) : sp( NULL ), stack( NULL ), size( 0 ) { }
-  jmp_buf env;  // the execution environment captured by set_jmp( )
-  void* sp;     // the stack pointer 
-  void* stack;  // the temporary space to maintain the latest stack contents
-  int size;     // the size of the stack contents
+  TCB() : sp(NULL), stack(NULL), size(0) {}
+  jmp_buf env; // the execution environment captured by set_jmp( )
+  void *sp;    // the stack pointer
+  void *stack; // the temporary space to maintain the latest stack contents
+  int size;    // the size of the stack contents
 };
-static TCB* cur_tcb;   // the TCB of the current thread in execution
+static TCB *cur_tcb; // the TCB of the current thread in execution
 
 // The queue of active threads
-static queue<TCB*> thr_queue;
+static queue<TCB *> thr_queue;
 
 // Alarm caught to switch to the next thread
 static bool alarmed = false;
-static void sig_alarm( int signo ) {
-  alarmed = true;
-}
+static void sig_alarm(int signo) { alarmed = true; }
 
 // A function to be executed by a thread
-void (*func)( void * );
+void (*func)(void *);
 void *args = NULL;
 static bool thread_created = false;
 
-static void scheduler( ) {
-  // invoke scheduler
-  if ( setjmp( scheduler_env ) == 0 ) {
+static void scheduler() {
+  // save the environment for the scheduler
+  if (setjmp(scheduler_env) == 0) {
     cerr << "scheduler: initialized" << endl;
-    if ( signal( SIGALRM, sig_alarm ) == SIG_ERR ) {
-      perror( "signal function" );
-      exit( -1 );
+
+    // set the function that gets called by the alarm signal
+    if (signal(SIGALRM, sig_alarm) == SIG_ERR) {
+      // notify user of error with signal handler
+      perror("signal function");
+
+      // if we fail to set alarm function exit with error
+      exit(-1);
     }
-    longjmp( main_env, 1 );
+
+    // return to setjmp in scheduler_init
+    longjmp(main_env, 1);
   }
+  /**
+   * every time longjmp is called with the scheduler_env we are brought here
+   */
 
   // check if it was called from sthread_create( )
-  if ( thread_created == true ) {
+  if (thread_created == true) {
+    // lower created flag for other calls to scheduler environment
     thread_created = false;
-    ( *func )( args );
+
+    // call the created threads function and pass in arguments as well
+    (*func)(args);
   }
 
-  // restore the next thread's environment
-  if ( ( cur_tcb = thr_queue.front( ) ) != NULL ) {
-    thr_queue.pop( );
+  // grab the thread control block in front of queue
+  if ((cur_tcb = thr_queue.front()) != NULL) {
+    // remove tcb from front of queue
+    thr_queue.pop();
 
-    // allocate a time quontum of 5 seconds
-    alarm( 5 );
+    // allocate a time quantum of 5 seconds
+    alarm(5);
 
-    // return to the next thread's execution
-    longjmp( cur_tcb->env, 1 );
+    // go back to the execution of threads function
+    longjmp(cur_tcb->env, 1);
   }
 
   // no threads to schedule, simply return
   cerr << "scheduler: no more threads to schedule" << endl;
-  longjmp( main_env, 2 );
+  longjmp(main_env, 2);
 }
-
-
