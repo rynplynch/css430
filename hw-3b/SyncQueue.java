109debbdb5ad7713a656338e3818d6f1cafc6b69
Ryan Lynch <rynplynch@gmail.com>
2024-08-07 01:00:11 -0700

    solution for queue made up of conditions

--- hw-3b/SyncQueue.java
+++ hw-3b/SyncQueue.java
@@ -19,27 +19,36 @@ public class SyncQueue {
     public SyncQueue( ) {
         // TODO: implement this constructor
         // Assume SyncQueue( COND_MAX );
+        initQueue(COND_MAX);
     }
 
     public SyncQueue( int condMax ) {
         // TODO: implement this constructor
+        initQueue(condMax);
     }
 
     int enqueueAndSleep( int condition ) {
         // TODO: implement this method
         // Verify the correctness of condition.
-        // Call the corresponding queue[ ].sleep( ).
-        // Return the corresponding child thread ID.
+        if (condition > -1 && condition < queue.length)
+            // Call the corresponding queue[ ].sleep( ).
+            // Return the corresponding child thread ID.
+            return queue[condition].sleep();
+        return 0;
     }
 
     void dequeueAndWakeup( int condition, int tid ) {
         // TODO: implement this method
         // verify the correctness of condition.
-        // Call the corresponding queue[ ].wakeup( ... );
+        if (condition > -1 && condition < queue.length)
+            // Call the corresponding queue[ ].wakeup( ... );
+            queue[condition].wakeup(tid);
     }
 
     void dequeueAndWakeup( int condition ) {
         // TODO: implement this method
-        // Assume tid = 0.
+        if (condition > -1 && condition < queue.length)
+            // Assume tid = 0.
+            queue[condition].wakeup(0);
     }
 }
